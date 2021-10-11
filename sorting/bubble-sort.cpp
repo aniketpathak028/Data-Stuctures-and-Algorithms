c0a2e6d26f1827e2a21427c2e71e0276fb1db812
Aniket Pathak <aniketpathak028@gmail.com>
2021-10-11 08:50:14 +0530

    add sorting folder and bubble sort code

---
+++ sorting/bubble-sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void BubbleSort(vector<int> &v)
+{
+    for (int i = 1; i <= v.size() - 1; i++)
+    {
+        for (int j = 0; j <= v.size() - i - 1; j++)
+        {
+            if (v[j] > v[j + 1])
+            {
+                swap(v[j], v[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    vector<int> a;
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    cout << "Enter the array: \n";
+    while (n--)
+    {
+        int c;
+        cin >> c;
+        a.push_back(c);
+    }
+    BubbleSort(a);
+    cout << "The sorted array is: \n";
+    for (auto i : a)
+        cout << i << " ";
+}
